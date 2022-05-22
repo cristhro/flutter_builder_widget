6fe5c79c8b1a965422b46c48521941259dc0f59e
Cristhian <cristhian.4elit@gmail.com>
2022-05-22 18:24:32 +0200

    Added layout

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Layout extends StatelessWidget {
-  const Layout({Key key}) : super(key: key);
+  const Layout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text('Layout');
   }
 }
